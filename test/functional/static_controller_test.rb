5955ad04c1c1b8d4768d3d7155fb7bf048ea2e8a
Russ Schissler <russ@zipnil.com>
2011-06-22 19:12:29 -0600

    confirmed simple use of unit and functional testing in parallel with dao.

--- test/functional/static_controller_test.rb
+++ test/functional/static_controller_test.rb
@@ -5,4 +5,10 @@ class StaticControllerTest < ActionController::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "should get about" do
+    get 'about'
+    assert_response :success
+  end
+  
 end
--- test/unit/status_test.rb
+++ test/unit/status_test.rb
@@ -5,4 +5,14 @@ class StatusTest < ActiveSupport::TestCase
   test "the truth" do
     assert true
   end
+  
+  test "create a status" do
+    assert_difference 'Status.count',1 do
+        Status.create(:context_type => "Organization",
+        :context_id => 1,
+        :content => "Tacos and Tacos and Tacos - get them on the Hill."
+        )
+    end
+  end
+
 end
