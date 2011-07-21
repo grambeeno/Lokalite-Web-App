ea72afd2751bc3cf7aa0413e346f983f9a7c6bce
Zef Houssney <zef@madebykiwi.com>
2011-07-21 10:45:03 -0600

    Refactor HTTP Basic auth so we can send different data to people who are logged in on whitelisted URLs.

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -68,30 +68,18 @@ protected
   end
 
   def setup_api
-    if white_listed?(path)
-      @api = Api.new
-      return
+    user =  current_user || authenticate_with_http_basic do |email, password|
+      User.authenticate(:email => email, :password => password)
     end
 
-    email, password = http_basic_auth_info
-
-    if !email.blank? and !password.blank?
-      user = User.find_by_email(email)
-      if user.password == password
-        @api = Api.new(user)
+    if user
+      @api = user.api
+      return
+    else
+      if white_listed?(path)
+        @api = Api.new
       else
         render(:nothing => true, :status => :unauthorized)
-        return
-      end
-    else
-      if defined?(current_user)
-        if current_user
-          @api = Api.new(current_user)
-        else
-          render(:nothing => true, :status => :unauthorized)
-        end
-      else
-        @api = Api.new
       end
     end
   end
@@ -109,20 +97,6 @@ protected
     self.class.white_listed?(path)
   end
 
-  def http_basic_auth
-    @http_basic_auth ||= (
-      request.env['HTTP_AUTHORIZATION']   ||
-      request.env['X-HTTP_AUTHORIZATION'] ||
-      request.env['X_HTTP_AUTHORIZATION'] ||
-      request.env['REDIRECT_X_HTTP_AUTHORIZATION'] ||
-      ''
-    )
-  end
-
-  def http_basic_auth_info
-    username, password =
-      ActiveSupport::Base64.decode64(http_basic_auth.split.last.to_s).split(/:/, 2)
-  end
 end
 
 ApiController = APIController ### rails is a bitch - shut her up
