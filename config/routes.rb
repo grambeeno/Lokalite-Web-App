e2747d9a90edc62eea945a5d50956de4e51a3ec6
Zef Houssney <zef@madebykiwi.com>
2011-08-10 15:10:37 -0600

    Change root route

--- config/routes.rb
+++ config/routes.rb
@@ -79,7 +79,7 @@ Lokalite::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  root :to => "auth#signup"
+  root :to => "events#browse"
 end
 
 # The priority is based upon order of creation:
