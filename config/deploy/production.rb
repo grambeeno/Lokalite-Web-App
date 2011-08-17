47f9ef312c7be7f31f6ddb739c7ba85207e96123
Zef Houssney <zef@madebykiwi.com>
2011-08-17 13:34:50 -0600

    Update deployment configuration for multiple stages.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,2 +1,12 @@
 # PRODUCTION-specific deployment configuration
 # please put general deployment config in config/deploy.rb
+
+set :user, "lokalite"
+
+ip = 'lokalite.com'
+role :web, ip
+role :app, ip
+role :db,  ip, :primary => true
+
+set :deploy_to, "/home/#{user}/#{application}"
+
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,2 +1,12 @@
 # STAGING-specific deployment configuration
 # please put general deployment config in config/deploy.rb
+
+set :user, "lokalite"
+
+ip = '174.136.1.78'
+role :web, ip
+role :app, ip
+role :db,  ip, :primary => true
+
+set :deploy_to, "/home/#{user}/#{application}"
+
