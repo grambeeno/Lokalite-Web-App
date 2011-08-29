52caa48847888803befb978dad05d8cd2565e9a5
Zef Houssney <zef@madebykiwi.com>
2011-08-29 10:51:48 -0600

    Clarify location and organization for events/show.

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,3 +1,12 @@
 module EventsHelper
+  def event_location_string(event)
+    organization = link_to(event.organization.name, organization_path(@event.organization.slug, @event.organization.id))
 
+    if event.organization.name == event.location.name
+      string = "@ #{organization}"
+    else
+      string = "By #{organization} @ #{event.location.name}"
+    end
+    raw string
+  end
 end
