6b2efb81f3a266e7cd43ea3243f0ae6575d36200
Zef Houssney <zef@madebykiwi.com>
2011-09-14 11:22:44 -0600

    Rework migrations to make them work after changes from the previous commit.

--- db/migrate/20110913202911_add_suite_to_location.rb
+++ db/migrate/20110913202911_add_suite_to_location.rb
@@ -2,13 +2,6 @@ class AddSuiteToLocation < ActiveRecord::Migration
   def self.up
     add_column :locations, :suite, :string
     add_column :locations, :user_given_street, :string
-
-    Location.reset_column_information
-
-    for location in Location.all
-      location.user_given_street = location.street
-      location.save
-    end
   end
 
   def self.down
--- db/migrate/20110914161056_rename_street_to_geocoded_street.rb
+++ db/migrate/20110914161056_rename_street_to_geocoded_street.rb
@@ -1,6 +1,13 @@
 class RenameStreetToGeocodedStreet < ActiveRecord::Migration
   def self.up
     rename_column :locations, :street, :geocoded_street
+
+    Location.reset_column_information
+
+    for location in Location.all
+      location.user_given_street = location.geocoded_street
+      location.save
+    end
   end
 
   def self.down
