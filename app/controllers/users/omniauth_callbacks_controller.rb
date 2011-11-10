fee830a06f60cd7edec4fd340c8479ec8adab19e
Brett Goulder <brett.goulder@gmail.com>
2011-11-10 15:39:47 -0700

    Finally tracked down that tricky user access token

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,7 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def facebook
     @user = User.find_for_facebook_oauth(env["omniauth.auth"], current_user)
-
+    session[:user_access_token] = env['omniauth.auth']['credentials']['token']
     if @user.persisted?
       flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Facebook"
       sign_in_and_redirect @user, :event => :authentication
