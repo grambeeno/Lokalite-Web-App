4e13b4574dd16a366b12fba5a5a0666790f8fb09
Brett Goulder <brett.goulder@gmail.com>
2011-11-10 16:26:33 -0700

    Requests dialogue redirect

--- app/controllers/facebook_controller.rb
+++ app/controllers/facebook_controller.rb
@@ -1,13 +1,9 @@
 class FacebookController < ApplicationController
   
   def authorize
-    request_ids = params[:request_ids].to_a
-    if request_ids.count == 1
-      @invitation = PlanUserInvitation.find_by_request_id(request_ids.first)
-      @plan = Plan.find_by_uuid(@invitation.uuid) 
-    else
-      # render :index
-    end
+    request_ids = Array(params[:request_ids]).join(',').strip.split(/\s*,\s*/).first
+    @invitation = PlanUserInvitation.find_by_request_id(request_ids)
+    @plan = Plan.find_by_uuid(@invitation.uuid) 
   end
 
   def ajax_request_handler
