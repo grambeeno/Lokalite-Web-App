ab640141fb900b1f4227393e691a08cd4e2b121a
Graham <graham@ubuntu.(none)>
2012-01-17 09:24:33 -0700

    deployment to softlayer stuff

--- config/deploy/softlayer.rb
+++ config/deploy/softlayer.rb
@@ -1,9 +1,9 @@
-# DEVELOPMENT-specific deployment configuration
+# Production-specific deployment configuration
 # please put general deployment config in config/deploy.rb
 
 set :user, "lokalite"
 
-ip = '184.173.113.114'
+ip = '184.173.113.115'
 role :web, ip
 role :app, ip
 role :db,  ip, :primary => true
