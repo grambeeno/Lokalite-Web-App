74b495d3dbe8e5912d588298e6bbeadc833b9643
Graham <graham@ubuntu.(none)>
2012-02-01 09:03:50 -0700

    redirect for mobile

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Lokalite::Application.routes.draw do
 
   devise_for :users, :controllers => { :omniauth_callbacks => 'users/omniauth_callbacks' }
 
+  match '/' => 'events#index', :constraints => { :subdomain => /m/ }, :mobile => '1', :category => 'featured', :origin => 'boulder-colorado'
   match '/' => 'events#index', :constraints => { :subdomain => /list/ }, :fbview => '1', :category => 'featured', :origin => 'boulder-colorado'
   match '/' => 'slides#show', :constraints => { :subdomain => /information/ }
   match '/business' => 'root#business', :as => 'business_promo'
