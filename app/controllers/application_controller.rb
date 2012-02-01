6f5c843867298f29661ea2fd09201f4e6a62b5fd
Graham <graham@ubuntu.(none)>
2012-02-01 12:32:48 -0700

    redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,7 +36,7 @@ end
   def prepare_for_mobile
     session[:mobile_param] = params[:mobile] if params[:mobile]
     request.format = :mobile if mobile_device?
-    redirect_to 'http://m.lokalite.com' if mobile_device?
+    redirect_to 'http://m.lokalite.com' if session[:mobile_param] == '1'
 end
 
   def fb?
