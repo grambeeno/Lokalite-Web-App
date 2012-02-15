b112ee799c8a4dd614fd5c3adf39e52c9115885d
Graham <graham@ubuntu.(none)>
2012-02-15 10:13:57 -0700

    mobile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,8 +24,12 @@ protected
     end
   end
   def mobile_device?
+    if session[:mobile_param]
+       session[:mobile_param] == '1'
+    else
        request.user_agent =~ /Mobile|webOS|AvantGo|Dolphin|OpenWave|Plucker|NetFront|PIE|AT&T|RiM|9xxx Series|88xx Series|Cricket|HP|HTC|LGE|Motorola/
-  end
+    end
+end
 
   helper_method :mobile_device?
 
