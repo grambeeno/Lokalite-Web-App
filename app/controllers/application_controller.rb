bc56015e64733858e269689a169f07594bd05060
Will Powers <will@lokalite.com>
2012-11-18 14:53:29 -0700

    bw mobile fix #2w

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,8 @@ protected
   def mobile_device?
     if session[:mobile_param]
        session[:mobile_param] == '1'     
+    elsif session[:mobile_param] and boulder_weekly?
+       session[:mobile_param] == '0'
     else
        request.user_agent =~ /Mobile|webOS|AvantGo|Dolphin|OpenWave|Plucker|NetFront|PIE|AT&T|RiM|9xxx Series|88xx Series|Cricket|Dell|Googlebot-Mobile|HP|HTC|LGE|Motorola/
     end
@@ -53,7 +55,7 @@ end
   def prepare_for_mobile
     session[:mobile_param] = params[:mobile] if params[:mobile]
     if mobile_device?
-      request.format = :mobile unless boulderweekly?
+      request.format = :mobile
     end
 #    if mobile_device? && request.subdomain != 'm'
 #      redirect_to 'http://m.lokalite.com'
