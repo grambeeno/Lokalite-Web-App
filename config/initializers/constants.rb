43a4f0b2480c6ee2f1c64940e6f00ef8adc4d197
Will Powers <will@lokalite.com>
2012-11-17 19:57:58 -0700

    boulderweekly/facebook fix #20

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -3,7 +3,7 @@ if Rails.env.development?
   FACEBOOK_APP_ID     = '221397624592914'
   FACEBOOK_APP_SECRET = '74edaeb242da1bdb0a8c2f8dfd055b19'
 else 
-  if Rails.env.production? && 'events.boulderweekly.com' 
+  if Rails.env.production? && self.boulder_weekly?
     REPORT_GRID_TOKEN = 'D90AEC76-FAC1-4591-8726-2126832BD18D'
     FACEBOOK_APP_ID     = '443689675690876'
     FACEBOOK_APP_SECRET = '5bec3517b5649afab095b49f01e8e688'
