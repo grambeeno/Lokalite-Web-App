1549221c5e87162aa1cb3ce757b5b69154bde5b2
Will Powers <will@lokalite.com>
2013-01-04 19:45:59 -0700

    commenting out date rage for download file until it doesn't effect website

--- app/models/event.rb
+++ app/models/event.rb
@@ -145,11 +145,11 @@ class Event < ActiveRecord::Base
 
     results = Event.approved.after(start_time)
     results = results.before(latest_start) if latest_start
-    if :export_to_csv       
+    #if :export_to_csv       
       # Date range for Boulder Weekly to export only a week's worth of data... They will download the file on Tuesday and the file will provide data for
-      # Thursday until next Wednesday... This breaks the All Events category which breaks the iPhone App Shit!`
-      results = Event.approved.after(Date.today + 2.days).before(Date.today + 8.days) 
-    end
+      # Thursday until next Wednesday
+      #results = Event.approved.after(Date.today + 2.days).before(Date.today + 8.days) 
+    #end
 
     organization_id = options[:organization_id]
 
