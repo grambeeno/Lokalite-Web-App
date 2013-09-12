6fe2449dbbc365ca78866983c40aa68a5697588f
Will Powers <will@lokalite.com>
2013-09-11 22:06:49 -0600

    fixing some redirects when adding the fullsite back to mobile #3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -46,15 +46,15 @@ end
 
   def prepare_for_BW
     if boulder_weekly? && params[:controller] == 'root'
-      redirect_to events_path(:origin => params[:origin], :category => 'featured')
-      session[:skip_landing_page] = true
+      redirect_to events_path(:origin => params[:origin], :category => 'featured') unless mobile_device? 
     end
   end
 
   def prepare_for_mobile
     session[:mobile_param] = params[:mobile] if params[:mobile]
     if mobile_device?
-      request.format = :mobile  
+      request.format = :mobile 
+      session[:skip_landing_page] = true
     end
 #    if mobile_device? && request.subdomain != 'm'
 #      redirect_to 'http://m.lokalite.com'
