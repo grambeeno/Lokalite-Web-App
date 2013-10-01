9c6076fe1b5c7d881d2bbdc487c2756c70e05ba6
Zef Houssney <zef@madebykiwi.com>
2013-09-30 21:52:13 -0600

    Update gems to enable geocoding. Remove google geocoding api key.

--- config/environment.rb
+++ config/environment.rb
@@ -4,5 +4,5 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 Lokalite::Application.initialize!
 
-Geocode.geocoder = Graticule.service(:google).new('ABQIAAAAy20eG1RZkih79hiJ3rMHiRSULVwnM-lDnnuK0XBsp3zStsgwIBTeJ-UvgBzSoJdYL6rQjmLu_lcHjA')
+Geocode.geocoder = Graticule.service(:google).new('')
 
